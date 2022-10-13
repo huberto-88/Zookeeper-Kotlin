277d764dd70ef3ff68099ca633384ea4df3a3af1
Hubert <huberto-88@o2.pl>
2022-10-13 19:01:02 +0200

    stage 2/4 passed

--- Zookeeper/task/src/Zookeeper.kt
+++ Zookeeper/task/src/Zookeeper.kt
@@ -1,9 +1,24 @@
+var camel = """Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+
 fun main() {
-    println("""
-        I love animals!
-        Let's check on the animals...
-        The deer looks fine.
-        The bat looks happy.
-        The lion looks healthy.
-    """.trimIndent())
+    println(camel)
 }
